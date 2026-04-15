7ab74cda41b3f4e7aa5c06832aa6fadad1604201
ubuntu <ubuntu@dev>
2026-04-15 16:06:05 +0200

    Fix: use 0x00 FileOption to actually disable SDM in resetNdefOnly

--- src/bolt.h
+++ src/bolt.h
@@ -459,7 +459,7 @@ public: // Access specifier
       Serial.println(F("Authentication successful (factory zero key)."));
 
       Serial.println(F("Disabling SDM and resetting file settings..."));
-      uint8_t fileSettings[] = {0x40, 0xE0, 0xEE, 0x01, 0xFF, 0xFF};
+      uint8_t fileSettings[] = {0x00, 0xE0, 0xEE, 0x01, 0xFF, 0xFF};
       nfc->ntag424_ChangeFileSettings((uint8_t)2, fileSettings,
                                       (uint8_t)sizeof(fileSettings),
                                       (uint8_t)NTAG424_COMM_MODE_FULL);
@@ -539,7 +539,7 @@ public: // Access specifier
         if (authenticated == 1) {
           Serial.println("Authentication successful.");
           Serial.println("Disable Mirroring and SDM.");
-          uint8_t fileSettings[] = {0x40, 0xE0, 0xEE, 0x01, 0xFF, 0xFF};
+      uint8_t fileSettings[] = {0x00, 0xE0, 0xEE, 0x01, 0xFF, 0xFF};
 
           nfc->ntag424_ChangeFileSettings((uint8_t)2, fileSettings,
                                           (uint8_t)sizeof(fileSettings),
